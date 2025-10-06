0acac3eb7ca0800c86647d1c8aa3dca03e75f7a3
오영훈 <skek241280@gmail.com>
2025-10-07 02:13:46 +0900

    feat: 미션 및 가게 도메인의 엔티티 별 레포 추가

---
+++ src/main/java/com/example/umc9th/domain/store/repository/StoreRepository.java
@@ -0,0 +1,7 @@
+package com.example.umc9th.domain.mission.repository;
+
+import com.example.umc9th.domain.mission.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store,Long> {
+}
