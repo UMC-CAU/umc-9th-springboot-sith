029b0f5dc04570572b751fe7b739df02b1e15c96
오영훈 <skek241280@gmail.com>
2025-10-07 01:29:01 +0900

    feat: 미션 엔티티 설정 및 초기화

---
+++ src/main/java/com/example/umc9th/domain/mission/entity/Mission.java
@@ -0,0 +1,29 @@
+package com.example.umc9th.domain.mission.entity;
+
+import com.example.umc9th.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name ="mission")
+public class Mission extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description", nullable = false, length = 300)
+    private String description;
+
+    @Column(name = "point",nullable = false)
+    private int point;
+
+    @Column(name = "deadline",nullable = false)
+    private LocalDateTime deadline;
+
+}
