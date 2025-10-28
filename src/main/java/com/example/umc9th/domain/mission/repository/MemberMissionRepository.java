e14c2588b49484abc67d29be91bc253215f1a8d0
오영훈 <skek2412@naver.com>
2025-10-28 18:51:45 +0900

    feat: MemberMissionRepository에 커스텀 레포 상속 추가

--- src/main/java/com/example/umc9th/domain/mission/repository/MemberMissionRepository.java
+++ src/main/java/com/example/umc9th/domain/mission/repository/MemberMissionRepository.java
@@ -3,5 +3,5 @@ package com.example.umc9th.domain.mission.repository;
 import com.example.umc9th.domain.mission.entity.mapping.MemberMission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberMissionRepository extends JpaRepository<MemberMission,Long> {
+public interface MemberMissionRepository extends JpaRepository<MemberMission,Long>, MemberMissionRepositoryCustom {
 }
