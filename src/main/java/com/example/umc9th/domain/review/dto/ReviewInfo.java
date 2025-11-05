da5a57a65ca51b1e4fd8babdfd7a685970a6624a
오영훈 <skek2412@naver.com>
2025-11-05 20:32:36 +0900

    feat: 리뷰에 보여줄 데이터 dto 추가

---
+++ src/main/java/com/example/umc9th/domain/review/dto/ReviewInfo.java
@@ -0,0 +1,20 @@
+package com.example.umc9th.domain.review.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewInfo {
+    private Long reviewId;
+    private String storeName;
+    private double rating;
+    private String content;
+    private List<String> photoUrls;
+    private LocalDateTime updatedAt;
+}
