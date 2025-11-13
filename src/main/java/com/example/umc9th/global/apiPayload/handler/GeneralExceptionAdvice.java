4c649350fe5b73fc0ec5f1db7657f4036ed2de26
오영훈 <skek2412@naver.com>
2025-11-13 11:59:40 +0900

    feat: GeneralExceptionAdvice 추가

---
+++ src/main/java/com/example/umc9th/global/apiPayload/handler/GeneralExceptionAdvice.java
@@ -0,0 +1,23 @@
+package com.example.umc9th.global.apiPayload.handler;
+
+import com.example.umc9th.global.apiPayload.ApiResponse;
+import com.example.umc9th.global.apiPayload.code.BaseErrorCode;
+import com.example.umc9th.global.apiPayload.code.GeneralErrorCode;
+import com.example.umc9th.global.apiPayload.exception.GeneralException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GeneralExceptionAdvice {
+    @ExceptionHandler(GeneralException.class)
+    public ResponseEntity<ApiResponse<Void>> handlerException(GeneralException ex){
+        return ResponseEntity.status(ex.getCode().getStatus()).body(ApiResponse.onFailure(ex.getCode(), null));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<String>> handlerException(Exception ex){
+        BaseErrorCode code = GeneralErrorCode.INTERNAL_SERVER_ERROR;
+        return ResponseEntity.status(code.getStatus()).body(ApiResponse.onFailure(code,ex.getMessage()));
+    }
+}
