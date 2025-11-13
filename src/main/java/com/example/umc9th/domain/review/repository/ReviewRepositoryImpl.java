ea70fc441ccb0a35756d204d108f1a1b236b9721
오영훈 <skek2412@naver.com>
2025-11-13 11:57:03 +0900

    feat: @RequiredArgsConstructor 추가 및 예외발생 추가

--- src/main/java/com/example/umc9th/domain/review/repository/ReviewRepositoryImpl.java
+++ src/main/java/com/example/umc9th/domain/review/repository/ReviewRepositoryImpl.java
@@ -1,14 +1,18 @@
 package com.example.umc9th.domain.review.repository;
 
+import com.example.umc9th.domain.member.repository.MemberRepository;
 import com.example.umc9th.domain.review.dto.ReviewInfo;
 import com.example.umc9th.domain.review.dto.ReviewReplyInfo;
 import com.example.umc9th.domain.review.entity.QReview;
 import com.example.umc9th.domain.review.entity.QReviewPhoto;
 import com.example.umc9th.domain.review.entity.QReviewReply;
+import com.example.umc9th.domain.review.exception.ReviewException;
+import com.example.umc9th.domain.review.exception.code.ReviewErrorCode;
 import com.example.umc9th.domain.store.entity.QStore;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,16 +21,16 @@ import static com.querydsl.core.group.GroupBy.groupBy;
 import static com.querydsl.core.group.GroupBy.list;
 
 @Repository
+@RequiredArgsConstructor
 public class ReviewRepositoryImpl implements ReviewRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
-
-    public ReviewRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
-        this.jpaQueryFactory = jpaQueryFactory;
-    }
+    private final MemberRepository memberRepository;
 
     @Override
     public List<ReviewInfo> findMyReviews(Long memberId,Predicate predicate) {
+        memberRepository.findById(memberId).orElseThrow(()->new ReviewException(ReviewErrorCode.NO_MEMBER));
+
         QReview r = QReview.review;
         QStore s = QStore.store;
         QReviewPhoto rp = QReviewPhoto.reviewPhoto;
