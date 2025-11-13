5c0de36b95440e6aea1a9b0a3c66b1ff6a790de9
오영훈 <skek2412@naver.com>
2025-11-13 11:27:31 +0900

    refactor: 파라미터 타입 래퍼 타입으로 통일

--- src/main/java/com/example/umc9th/domain/mission/repository/MemberMissionRepositoryCustom.java
+++ src/main/java/com/example/umc9th/domain/mission/repository/MemberMissionRepositoryCustom.java
@@ -1,16 +1,16 @@
 package com.example.umc9th.domain.mission.repository;
 
 
-import com.example.umc9th.domain.mission.dto.SelectedMissionsInfo;
+import com.example.umc9th.domain.mission.dto.SelectedMissionInfo;
 import com.example.umc9th.domain.mission.dto.UnselectedMissionInfo;
 
 import java.util.List;
 
 
 public interface MemberMissionRepositoryCustom {
-    List<SelectedMissionsInfo> findSelectedMissionsWithCursor(Long memberId, String cursor, boolean isCompleted,int pageSize);
+    List<SelectedMissionInfo> findSelectedMissionsWithCursor(Long memberId, String cursor, Boolean isCompleted, Integer pageSize);
 
     Long findCompletedMissionCountByDistrict(Long memberId,String district);
 
-    List<UnselectedMissionInfo> findUnselectedMissionsByDistrictWithCursor(Long memberId, String cursor, String district, int pageSize);
+    List<UnselectedMissionInfo> findUnselectedMissionsByDistrictWithCursor(Long memberId, String cursor, String district, Integer pageSize);
 }
