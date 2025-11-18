8a8a8947a1e921462b58a95b39061f74f99dfb81
오영훈 <skek2412@naver.com>
2025-11-18 11:31:39 +0900

    feat: 회원가입 toDTO, toMember 추가

--- src/main/java/com/example/umc9th/domain/member/converter/MemberConverter.java
+++ src/main/java/com/example/umc9th/domain/member/converter/MemberConverter.java
@@ -1,5 +1,6 @@
 package com.example.umc9th.domain.member.converter;
 
+import com.example.umc9th.domain.member.dto.req.MemberReqDTO;
 import com.example.umc9th.domain.member.dto.res.MemberResDTO;
 import com.example.umc9th.domain.member.entity.Member;
 
@@ -11,6 +12,21 @@ public class MemberConverter {
                 .point(m.getPoint())
                 .profileUrl(m.getProfileUrl())
                 .phoneNum(m.getPhoneNumber())
+                .build();
+    }
+
+    public static MemberResDTO.JoinDTO toJoinDTO(Member member){
+        return MemberResDTO.JoinDTO.builder()
+                .memberId(member.getId())
+                .createdAt(member.getCreatedAt())
+                .build();
+    }
+
+    public static Member toMember(MemberReqDTO.JoinDTO dto){
+        return Member.builder()
+                .name(dto.name())
+                .address(dto.address())
+                .birth(dto.birth())
                 .build();
     }
 }
