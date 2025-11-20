83db1c0d2c7fab78ca8deda0c1fd8c73a40fb789
오영훈 <skek2412@naver.com>
2025-11-20 16:19:10 +0900

    feat: 들여쓰기 수정 & 리뷰 생성 요청, 응답 dto 추가

--- src/main/java/com/example/umc9th/domain/review/converter/ReviewConverter.java
+++ src/main/java/com/example/umc9th/domain/review/converter/ReviewConverter.java
@@ -1,6 +1,7 @@
 package com.example.umc9th.domain.review.converter;
 
 import com.example.umc9th.domain.review.dto.ReviewInfo;
+import com.example.umc9th.domain.review.dto.req.ReviewReqDTO;
 import com.example.umc9th.domain.review.dto.res.ReviewResDTO;
 import com.example.umc9th.domain.review.entity.Review;
 
@@ -8,14 +9,23 @@ import java.util.List;
 
 public class ReviewConverter {
     public static ReviewResDTO.ReviewList toReviewDTO(List<ReviewInfo> reviews){
-        return ReviewResDTO.ReviewList.builder().reviews(reviews).build();
-    }
-
-    public static ReviewInfo toReviewInfo(Review review){
-        return ReviewInfo.builder().reviewId(review.getId()).content(review.getContent()).rating(review.getRating())
-                .storeName(review.getStore().getName()).updatedAt(review.getUpdatedAt()).build();
+        return ReviewResDTO.ReviewList.builder()
+                .reviews(reviews)
+                .build();
     }
     public static ReviewResDTO.Exception toExceptionDTO(String exception){
         return ReviewResDTO.Exception.builder().exception(exception).build();
+    }
+    public static ReviewResDTO.ReviewCreateResDTO toCreateResDTO(Review review){
+        return ReviewResDTO.ReviewCreateResDTO.builder()
+                .reviewId(review.getId())
+                .createdAt(review.getCreatedAt())
+                .build();
+    }
+    public static Review toReview(ReviewReqDTO.ReviewReqInfo dto){
+        return Review.builder()
+                .content(dto.content())
+                .rating(dto.rating())
+                .build();
     }
 }
