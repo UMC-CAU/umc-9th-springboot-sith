9fdec1f3c41ea93e37fa5a0208a978d9ea73a0ca
오영훈 <skek2412@naver.com>
2025-11-24 21:19:13 +0900

    feat: 페이지네이션을 위해서 myReviewListDTO로 변경 & 가게 별 리뷰 보기 메소드 매핑

--- src/main/java/com/example/umc9th/domain/review/Controller/ReviewController.java
+++ src/main/java/com/example/umc9th/domain/review/Controller/ReviewController.java
@@ -1,39 +1,51 @@
 package com.example.umc9th.domain.review.Controller;
 
-import com.example.umc9th.domain.review.converter.ReviewConverter;
-import com.example.umc9th.domain.review.dto.ReviewInfo;
 import com.example.umc9th.domain.review.dto.req.ReviewReqDTO;
 import com.example.umc9th.domain.review.dto.res.ReviewResDTO;
 import com.example.umc9th.domain.review.service.command.ReviewCommandService;
 import com.example.umc9th.domain.review.service.query.ReviewQueryService;
+import com.example.umc9th.global.annotation.ValidPageNumber;
 import com.example.umc9th.global.apiPayload.ApiResponse;
 import com.example.umc9th.global.apiPayload.code.GeneralSuccessCode;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api")
-public class ReviewController {
+@Validated
+public class ReviewController implements ReviewControllerDocs{
 
     private final ReviewQueryService reviewQueryService;
     private final ReviewCommandService reviewCommandService;
 
+    @Override
     @GetMapping("/my-reviews")
-    public ApiResponse<ReviewResDTO.ReviewList> findReviews(
-            @RequestParam Long id, @RequestParam String query, @RequestParam String type){
-        List<ReviewInfo> result = reviewQueryService.findMyReviews(id,query,type);
+    public ApiResponse<ReviewResDTO.myReviewListDTO> findMyReviews(
+            @RequestParam Long id, @RequestParam String query,
+            @RequestParam String type, @ValidPageNumber @RequestParam Integer page)
+    {
+        ReviewResDTO.myReviewListDTO result = reviewQueryService.findMyReviews(id, query, type, page);
         GeneralSuccessCode code = GeneralSuccessCode.OK;
-        return ApiResponse.onSuccess(code, ReviewConverter.toReviewDTO(result));
+        return ApiResponse.onSuccess(code, result);
     }
 
     @PostMapping("/reviews")
     public ApiResponse<ReviewResDTO.ReviewCreateResDTO> createReview(@RequestBody @Valid ReviewReqDTO.ReviewReqInfo request){
         ReviewResDTO.ReviewCreateResDTO result = reviewCommandService.createReview(request);
         GeneralSuccessCode code = GeneralSuccessCode.CREATED;
+        return ApiResponse.onSuccess(code,result);
+    }
+
+    @Override
+    @GetMapping("/reviews")
+    public ApiResponse<ReviewResDTO.ReviewPreViewListDTO> getReviews(
+            @RequestParam String storeName, @RequestParam @ValidPageNumber Integer page
+    ){
+        ReviewResDTO.ReviewPreViewListDTO result = reviewQueryService.findReviews(storeName, page);
+        GeneralSuccessCode code = GeneralSuccessCode.OK;
         return ApiResponse.onSuccess(code,result);
     }
 }
