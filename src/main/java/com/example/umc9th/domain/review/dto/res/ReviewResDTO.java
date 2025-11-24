053a98e9ac4a2ab1d4822dfd5630b2385b3b3fab
오영훈 <skek2412@naver.com>
2025-11-24 21:19:43 +0900

    feat: 페이지네이션을 위해서 myReviewListDTO로 변경 & 가게 별 리뷰 보기 dto추가

--- src/main/java/com/example/umc9th/domain/review/dto/res/ReviewResDTO.java
+++ src/main/java/com/example/umc9th/domain/review/dto/res/ReviewResDTO.java
@@ -3,17 +3,43 @@ package com.example.umc9th.domain.review.dto.res;
 import com.example.umc9th.domain.review.dto.ReviewInfo;
 import lombok.Builder;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class ReviewResDTO {
 
     @Builder
-    public record ReviewList(List<ReviewInfo> reviews){}
+    public record myReviewListDTO(
+            List<ReviewInfo> reviews,
+            Integer listSize,
+            Integer totalPage,
+            Long totalElements,
+            Boolean isFirst,
+            Boolean isLast
+            ){}
 
     @Builder
     public record Exception(String exception){}
 
     @Builder
     public record ReviewCreateResDTO(Long reviewId, LocalDateTime createdAt){}
+
+    @Builder
+    public record ReviewPreViewListDTO(
+            List<ReviewPreViewDTO> reviewList,
+            Integer listSize,
+            Integer totalPage,
+            Long totalElements,
+            Boolean isFirst,
+            Boolean isLast
+    ){}
+
+    @Builder
+    public record ReviewPreViewDTO(
+            String ownerNickname,
+            Double score,
+            String content,
+            LocalDate createdAt
+    ){}
 }
