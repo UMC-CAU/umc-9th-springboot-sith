278e17f22e28cc60b7966eaec65dde84b78e6435
오영훈 <skek2412@naver.com>
2025-11-25 17:26:56 +0900

    feat: swagger 명세서에 메소드 추가

--- src/main/java/com/example/umc9th/domain/mission/controller/MissionControllerDocs.java
+++ src/main/java/com/example/umc9th/domain/mission/controller/MissionControllerDocs.java
@@ -1,5 +1,6 @@
 package com.example.umc9th.domain.mission.controller;
 
+import com.example.umc9th.domain.mission.dto.req.MissionReqDTO;
 import com.example.umc9th.domain.mission.dto.res.MissionResDTO;
 import com.example.umc9th.global.annotation.ValidPageNumber;
 import com.example.umc9th.global.apiPayload.ApiResponse;
@@ -7,6 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface MissionControllerDocs {
@@ -23,4 +25,19 @@ public interface MissionControllerDocs {
     })
     ApiResponse<MissionResDTO.MissionPreviewListDTO> getMissions(
             @RequestParam String storeName, @RequestParam(defaultValue = "1") @ValidPageNumber Integer page);
+
+
+    @Operation(
+            summary = "내가 진행 중인 미션 목록 조회 api",
+            description = "내가 진행 중인 미션을 모두 조회합니다. 페이지네이션으로 제공합니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",
+                    description = "미션 데이터 목록과 페이지네이션 메타데이터가 성공적으로 반환됨"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400",
+                    description = "실패 코드와 메시지가 반환됨",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))
+            )
+    })
+    ApiResponse<MissionResDTO.SelectedMissionList> getSelectedMissions(
+            @Valid MissionReqDTO.MyMissionReqDTO request );
 }
