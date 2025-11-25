a22f72586ef6a85ee8ba05845fa514ac0c495b34
오영훈 <skek2412@naver.com>
2025-11-25 12:57:59 +0900

    feat: 가게명 유니크 제약 추가

--- src/main/java/com/example/umc9th/domain/store/entity/Store.java
+++ src/main/java/com/example/umc9th/domain/store/entity/Store.java
@@ -17,7 +17,7 @@ public class Store extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name",nullable = false,length = 20)
+    @Column(name = "name",nullable = false,length = 20,unique = true)
     private String name;
 
     @Column(name = "detail_address",nullable = false,length = 100)
