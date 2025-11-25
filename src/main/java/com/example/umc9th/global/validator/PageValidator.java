8fc4f9637f000febf5878f626defebfd987bd664
오영훈 <skek2412@naver.com>
2025-11-25 12:57:44 +0900

    feat: 페이지 유효성 검사 로직 변경

--- src/main/java/com/example/umc9th/global/validator/PageValidator.java
+++ src/main/java/com/example/umc9th/global/validator/PageValidator.java
@@ -13,9 +13,9 @@ public class PageValidator implements ConstraintValidator<ValidPageNumber,Intege
     public boolean isValid(Integer value, ConstraintValidatorContext context) {
         boolean isValid = true;
 
-        if(value < 0){
+        if(value <= 0){
             context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate("페이지는 0 이상 이어야 합니다.")
+            context.buildConstraintViolationWithTemplate("페이지 번호는 양수이어야 합니다.")
                     .addConstraintViolation();
             isValid = false;
         }
