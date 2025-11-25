200aba99b6d866391963bd9f49d22599c80930df
오영훈 <skek2412@naver.com>
2025-11-25 18:41:21 +0900

    feat: 미션 도메인 에러코드 추가

--- src/main/java/com/example/umc9th/domain/mission/exception/code/MissionErrorCode.java
+++ src/main/java/com/example/umc9th/domain/mission/exception/code/MissionErrorCode.java
@@ -9,8 +9,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum MissionErrorCode implements BaseErrorCode {
 
-    NO_MISSION(HttpStatus.NOT_FOUND,"MISSION_404_1","해당 미션이 존재하지 않습니다.");
-
+    NO_MISSION(HttpStatus.NOT_FOUND,"MISSION_404_1","해당 미션이 존재하지 않습니다."),
+    NO_MEMBER_MISSION(HttpStatus.NOT_FOUND,"MISSION_404_2","해당 멤버_미션이 존재하지 않습니다."),
+    ALREADY_COMPLETED(HttpStatus.CONFLICT,"MISSION_409_1","이미 완료된 미션입니다.");
     private final HttpStatus status;
     private final String code;
     private final String message;
