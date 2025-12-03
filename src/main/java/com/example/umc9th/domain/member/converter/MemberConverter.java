e4160282642f288e81663128284d53019d9f1b35
오영훈 <skek2412@naver.com>
2025-12-03 12:26:02 +0900

    feat: toMember에 이메일, 패스워드, 롤, 성별 추가 & toMyPageInfo에 이름으로 변경

--- src/main/java/com/example/umc9th/domain/member/converter/MemberConverter.java
+++ src/main/java/com/example/umc9th/domain/member/converter/MemberConverter.java
@@ -3,11 +3,12 @@ package com.example.umc9th.domain.member.converter;
 import com.example.umc9th.domain.member.dto.req.MemberReqDTO;
 import com.example.umc9th.domain.member.dto.res.MemberResDTO;
 import com.example.umc9th.domain.member.entity.Member;
+import com.example.umc9th.global.auth.enums.Role;
 
 public class MemberConverter {
     public static MemberResDTO.myPageInfo toMyPageInfo(Member m){
         return MemberResDTO.myPageInfo.builder()
-                .nickname(m.getNickname())
+                .name(m.getName())
                 .email(m.getEmail())
                 .point(m.getPoint())
                 .profileUrl(m.getProfileUrl())
@@ -22,11 +23,15 @@ public class MemberConverter {
                 .build();
     }
 
-    public static Member toMember(MemberReqDTO.JoinDTO dto){
+    public static Member toMember(MemberReqDTO.JoinDTO dto, String password, Role role){
         return Member.builder()
                 .name(dto.name())
+                .email(dto.email())
+                .password(password)
+                .role(role)
                 .address(dto.address())
                 .birth(dto.birth())
+                .gender(dto.gender())
                 .build();
     }
 }
