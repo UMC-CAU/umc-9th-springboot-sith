acb4963aeddf8fea508bfe4647f2834d2b7180b8
오영훈 <skek2412@naver.com>
2025-12-03 12:21:20 +0900

    feat: SecurityConfig 추가

---
+++ src/main/java/com/example/umc9th/global/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.umc9th.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    private final String[] allowUrls = {
+            "/sign-up",
+//            "/swagger-ui/**",
+//            "/swagger-resources/**",
+//            "/v3/api-docs/**",
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers(allowUrls).permitAll()
+                .requestMatchers("/swagger-ui/index.html").hasRole("ADMIN")
+                .anyRequest().authenticated())
+                .formLogin(form->form
+                        .defaultSuccessUrl("/",true)
+                        .permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .logout(logout->logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .permitAll()
+                );
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
