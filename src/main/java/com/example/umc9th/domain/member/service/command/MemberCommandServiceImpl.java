27592046acb8872d09477ae0be9c827fb71ea791
오영훈 <skek2412@naver.com>
2025-12-04 11:27:49 +0900

    feat: 로그인 메소드 추가 & 구현

--- src/main/java/com/example/umc9th/domain/member/service/command/MemberCommandServiceImpl.java
+++ src/main/java/com/example/umc9th/domain/member/service/command/MemberCommandServiceImpl.java
@@ -5,10 +5,14 @@ import com.example.umc9th.domain.member.dto.req.MemberReqDTO;
 import com.example.umc9th.domain.member.dto.res.MemberResDTO;
 import com.example.umc9th.domain.member.entity.Member;
 import com.example.umc9th.domain.member.entity.mapping.MemberFood;
+import com.example.umc9th.domain.member.exception.MemberException;
+import com.example.umc9th.domain.member.exception.code.MemberErrorCode;
 import com.example.umc9th.domain.member.repository.FoodRepository;
 import com.example.umc9th.domain.member.repository.MemberFoodRepository;
 import com.example.umc9th.domain.member.repository.MemberRepository;
 import com.example.umc9th.global.auth.enums.Role;
+import com.example.umc9th.global.auth.jwt.JwtUtil;
+import com.example.umc9th.global.auth.service.CustomUserDetails;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,6 +29,7 @@ public class MemberCommandServiceImpl implements MemberCommandService{
     private final FoodRepository foodRepository;
     private final MemberFoodRepository memberFoodRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     @Override
     @Transactional
@@ -43,5 +48,27 @@ public class MemberCommandServiceImpl implements MemberCommandService{
         }
         memberRepository.save(member);
         return MemberConverter.toJoinDTO(member);
+    }
+
+    @Override
+    public MemberResDTO.LoginDTO login(MemberReqDTO.LoginDTO dto){
+        // Member 조회
+        Member member = memberRepository.findByEmail(dto.email())
+                .orElseThrow(() -> new MemberException(MemberErrorCode.NO_MEMBER));
+
+        // 비밀번호 검증
+        if (!passwordEncoder.matches(dto.password(), member.getPassword())){
+            throw new MemberException(MemberErrorCode.INVALID);
+        }
+
+        // JWT 토큰 발급용 UserDetails
+        CustomUserDetails userDetails = new CustomUserDetails(member);
+
+        // 엑세스 토큰 발급
+        String accessToken = jwtUtil.createAccessToken(userDetails);
+
+        // DTO 조립
+        return MemberConverter.toLoginDTO(member, accessToken);
+
     }
 }
